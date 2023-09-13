90a1e8c3cd55b16baca45ad2121c99d3197897e0
suhailk750 <Suhailsuhu750@gmail.com>
2023-09-13 20:53:42 +0530

    Initial commit

---
+++ travelproject/credentials/views.py
@@ -0,0 +1,62 @@
+# from email import message
+#
+# from django.shortcuts import render
+# from django.http import HttpResponse
+
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user=auth.authenticate(
+            username=username,password=password
+        )
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,"invalid credential")
+            return redirect('login')
+
+    return render(request,'login.html')
+
+
+def register(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        c_password = request.POST['confirm_password']
+        if password==c_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'username taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'email taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(
+                username=username,
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                password=password
+                )
+                user.save()
+                return redirect('login')
+
+        else:
+            messages.info(request, 'password not matched')
+            return redirect('register')
+        return redirect('/')
+    return render(request,"register.html")
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
