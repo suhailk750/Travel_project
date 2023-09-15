020a441ea79e83a044cd243ca58329eb5f29aa78
suhailk750 <100553721+suhailk750@users.noreply.github.com>
2023-09-15 14:45:38 +0530

    Delete travelproject directory

--- travelproject/credentials/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-
-    path('register',views.register,name='register'),
-    # path("add/",views.addition,name='addition'),
-    # path('contact/',views.contact,name='contact')
-]
--- travelproject/travelapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-
-from.models import place,team
-
-admin.site.register(place)
-admin.site.register(team)
